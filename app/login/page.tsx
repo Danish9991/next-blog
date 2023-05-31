24f41a0e97cd442534931f8a09819b7939fe4298
macbook <danishmir9991@gmail.com>
2023-05-31 09:56:25 +0530

    enabled react-hot-toaster

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -5,18 +5,14 @@ import React, { useState, FormEvent } from 'react';
 import { useRouter } from 'next/navigation';
 import Link from 'next/link';
 import Input from '../../components/input/input';
-import { toast } from 'react-hot-toast'
-
 
 
 interface InitialStateProps {
-    name: string,
     email: string,
     password: string
 }
 
 const initialState: InitialStateProps = {
-    name: '',
     email: '',
     password: ''
 }
@@ -39,24 +35,19 @@ const page = () => {
      */
     const handleSubmit = (event: FormEvent) => {
         event.preventDefault();
-        
-        if(!state.email || !state.password || !state.name){
-          toast.success('provide all details')
-        }
 
     }
 
     return (
         <form className='text-center' onSubmit={handleSubmit}>
             <div className='flex flex-col justify-center h-[450px] w-[350px] mx-auto gap-2'>
-                <Input type='text' name='name' id='name' placeholder='name' value={state.name} onChange={handleChange} />
                 <Input type='email' name='email' id='email' placeholder='email' value={state.email} onChange={handleChange} />
                 <Input type='password' name='password' id='password' placeholder='password' value={state.password} onChange={handleChange} />
-                <input type='submit' value='Sign Up' />
+                <button type='submit'>Sign In</button>
             </div>
 
             <div>
-                <div>Do you have an account ? <Link href='/login'>Sign in</Link> </div>
+                <div>Don't you have an account ? <Link href='/register'>Sign in</Link> </div>
             </div>
         </form>
     )
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
+'use client'
+import { toast } from 'react-toastify';
+
 export default function Home() {
+  toast.success('provide all details');
   return (
     <div>
       <h1 className="text-red-500">Hello from Next</h1>
+      
     </div>
   )
 }
